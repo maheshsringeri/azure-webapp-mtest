4ca838e5388d26568b2c46e2d3ff33ef6174c318
mahesh-educo <mahesh@educosoft.com>
2024-01-06 18:48:24 +0530

    Added changes for staging

---
+++ Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_webapp_mtest.Pages
+{
+    public class Deployment_SlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
